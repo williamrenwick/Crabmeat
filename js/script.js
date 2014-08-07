d9ceb503b4b111efcd1d8fa946700d9a68d9b8b5
Bob Renwick <bob.renwick@gmail.com>
2014-08-08 00:27:19 +0100

    fixing the issue with dog showing up at the beginning

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready( function(){
 	var controller;
 	var $title = $("#titlewrapper");
 	var $headlineX = $title.width() / 2 ;
-	var $headlineY = $title.height() / 2; 
+	var $headlineY = $title.height() / 2;
 	var $scrollind = $("#scrolldown").width() / 2;
 
 	$title.css({"margin-top": - $headlineY, "margin-left": - $headlineX});
@@ -23,10 +23,10 @@ $(document).ready( function(){
 	controller = new ScrollMagic();
 		var tweenleft = TweenMax.fromTo("#left-claw", 0.3,
 					{rotation:0},
-					{rotation: 10, 
-					 repeat: -1, 
+					{rotation: 10,
+					 repeat: -1,
 					 yoyo: true,
-					 ease: Quad.easeInOut} 
+					 ease: Quad.easeInOut}
 					);
 		var scene = new ScrollScene({triggerElement:"#dear", duration: 700})
 					.setTween(tweenleft)
@@ -34,17 +34,17 @@ $(document).ready( function(){
 
 		var tweenright = TweenMax.fromTo("#right-claw", 0.3,
 					{rotation:0},
-					{rotation: 10, 
-					 repeat: -1, 
+					{rotation: 10,
+					 repeat: -1,
 					 yoyo: true,
-					 ease: Quad.easeInOut} 
+					 ease: Quad.easeInOut}
 					);
 
 		var scene = new ScrollScene({triggerElement:"#dear", duration: 700})
 					.setTween(tweenright)
 					.addTo(controller);
 
-		var title1out = TweenMax.fromTo("#titlewrapper", 0.7, 
+		var title1out = TweenMax.fromTo("#titlewrapper", 0.7,
 					{"left": "50%"},
 					{"left": "-50%"}
 					);
@@ -53,44 +53,31 @@ $(document).ready( function(){
 					.setTween(title1out)
 					.addTo(controller);
 
-		var title2in = TweenMax.fromTo("#titlewrapper2", 0.7, 
-					{"left": "150%"},
-					{"left": "50%", onComplete: complete(2)}
-					);
+		var title2in = TweenMax.fromTo("#titlewrapper2", 0.7,
+			{"left": "150%"},
+			{"left": "50%" }
+		);
 
 		var scene = new ScrollScene({triggerElement: "#trigger1", duration: 600})
 					.setTween(title2in)
 					.addTo(controller);
 
-		var title2out = TweenMax.fromTo("#titlewrapper2", 0.7, 
-					{"left": "50%"},
-					{"left": "-50%"}
-					);
+		var title2out = TweenMax.to("#titlewrapper2", 0.7,
+			{ "left": "-50%" }
+		);
 
-		var scene = new ScrollScene({triggerElement: "#trigger3", duration: 600})
+		var scene = new ScrollScene({triggerElement: "#trigger2", duration: 600})
 					.setTween(title2out)
 					.addTo(controller)
 
-		var title3in = TweenMax.fromTo("#titlewrapper3", 0.7, 
-					{"left": "150%"},
-					{"left": "50%", onComplete: complete(3)}
-					);
+		var title3in = TweenMax.fromTo("#titlewrapper3", 0.7,
+			{"left": "150%"},
+			{"left": "50%" }
+		);
 
-		var scene = new ScrollScene({triggerElement: "#trigger3", duration: 600})
-					.setTween(title3in)
-					.addTo(controller);
-
-		function complete(n) {
-			if (n === 2) {
-				var title2Pos = $('#titlewrapper2').position.left;
-			} else if (n === 3) {
-				var title3Pos = $('#titlewrapper3').position.left;
-			} else { 
-				//do nothing
-			}
-		}
-
-}
+		var scene = new ScrollScene({triggerElement: "#trigger2", duration: 600})
+			.setTween(title3in)
+			.addTo(controller);
 
 })
 
